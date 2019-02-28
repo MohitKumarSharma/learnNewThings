60992ded685778042fc7d60486ca68bc1016b4ac
MohitKumarSharma <mohitkumar.t2@gmail.com>
2019-02-28 10:24:58 +0530

    Angular study related to dynamic and rouing

--- learnNode/FirstNodeTest.js
+++ learnNode/FirstNodeTest.js
@@ -2,10 +2,14 @@ console.log("This is first test");
 console.log("trying to create node server");
 
 var http = require('http');
+var url = require('url');
+var dtModule = require('./myFirstModule');
 
 http.createServer(function(req, resp) {
 	resp.writeHead(200, {'Content-Type': 'text/html'});
-	resp.end("Bhuck Bosdike!");
+	resp.write("Current time is " + dtModule.myDateTime());
+	resp.write(req.url);
+	resp.end("Bhuck Bosdike!" );
 }).listen(8080);
 
 console.log("Well done finally run server");
